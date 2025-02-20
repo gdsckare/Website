0b3e3a90b2171c40e586a89f732386c28d2d8835
ARUNKUMAR-0984 <arunkumars97462@gmail.com>
2025-02-21 00:23:18 +0530

    Your commit message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,16 @@ import Hero from "./Hero";
 import Features from "./Features";
 import Events from "./Events";
 import Team from "./Team";
+import Upcoming from "./Upcoming";
+
+
 
 function Home() {
   return (
     <>
       <Hero />
       <Features />
+      <Upcoming/>
       <Events />
       <Team />
     </>
